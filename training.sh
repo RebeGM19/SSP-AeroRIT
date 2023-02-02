e59d7b36f704bb53105c6a124184a26027a7d15d
Rebeca García <rebegarcia12@hotmail.com>
2023-02-02 13:11:17 +0100

    Add files via upload

---
+++ training.sh
@@ -0,0 +1,24 @@
+# python3 train.py --network_arch SSP --batch-size 32 --ngf 4 --parallel
+# python3 train.py --network_arch NoSSP --batch-size 32 --ngf 4 --parallel
+# python3 train.py --network_arch SSP --batch-size 64 --ngf 4 --parallel
+# python3 train.py --network_arch NoSSP --batch-size 64 --ngf 4 --parallel
+# 
+# python3 train.py --network_arch SSP --batch-size 32 --ngf 8 --parallel
+# python3 train.py --network_arch NoSSP --batch-size 32 --ngf 8 --parallel
+# python3 train.py --network_arch SSP --batch-size 64 --ngf 8 --parallel
+# python3 train.py --network_arch NoSSP --batch-size 64 --ngf 8 --parallel
+# 
+# python3 train.py --network_arch SSP --batch-size 32 --ngf 16 --parallel
+# python3 train.py --network_arch NoSSP --batch-size 32 --ngf 16 --parallel
+# python3 train.py --network_arch SSP --batch-size 64 --ngf 16 --parallel
+# python3 train.py --network_arch NoSSP --batch-size 64 --ngf 16 --parallel
+# 
+# python3 train.py --network_arch SSP --batch-size 32 --ngf 32 --parallel
+python3 train.py --network_arch NoSSP --batch-size 32 --ngf 32 --parallel
+python3 train.py --network_arch SSP --batch-size 64 --ngf 32 --parallel
+python3 train.py --network_arch NoSSP --batch-size 64 --ngf 32 --parallel
+
+python3 train.py --network_arch SSP --batch-size 32 --ngf 64 --parallel
+python3 train.py --network_arch NoSSP --batch-size 32 --ngf 64 --parallel
+python3 train.py --network_arch SSP --batch-size 64 --ngf 64 --parallel
+python3 train.py --network_arch NoSSP --batch-size 64 --ngf 64 --parallel
